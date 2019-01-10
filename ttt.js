be5145798f47d936b9ff4bda1d632f22f01f6626
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2019-01-10 13:29:52 -0600

    Set timeout function to clear board

--- ttt.js
+++ ttt.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	let addX = "<h1>X</h1>"
 	let addO = "<h1>O</h1>"
-	let index;
 	let turn = []
 	let board = [$("#one"), $("#two"), $("#three"), $("#four"), $("#five"),$("#six"), $("#seven"), $("#eight"), $("#nine")]
 	let combos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
@@ -17,15 +16,16 @@ $(document).ready(function() {
 			for(let i = 0; i < combos.length; i++){
 				if (turn.join("").includes(combos[i][0] + 'O') && turn.join("").includes(combos[i][1] + 'O') && turn.join("").includes(combos[i][2] + 'O') ){
 					alert('O IS WINNER!')
-					location.reload();
+					setTimeout(function() {$("#ttt_table tbody tr td").html(""); }, 1500);
+					turn.length = 0
 				} else if(turn.join("").includes(combos[i][0] + 'X') && turn.join("").includes(combos[i][1] + 'X') && turn.join("").includes(combos[i][2] + 'X') ){
 					alert('X IS WINNER!')
-					turn.length === 0
-					location.reload();
-				} else if (turn.length === 9){
+					setTimeout(function() {$("#ttt_table tbody tr td").html(""); }, 1500);
 					turn.length = 0
+				} else if (turn.length === 9){
 					alert('DRAW!')
-					location.reload();
+					setTimeout(function() {$("#ttt_table tbody tr td").html(""); }, 1500);
+					turn.length = 0
 				}
 			}
 		})
